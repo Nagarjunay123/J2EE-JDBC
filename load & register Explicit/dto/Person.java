db5bc5dda911d58365bcfe5cb1612b5e56be4eb0
Nagarjuna Yadavalli <86463176+Nagarjunay123@users.noreply.github.com>
2024-04-13 13:11:33 +0530

    Add files via upload

---
+++ load & register Explicit/dto/Person.java
@@ -0,0 +1,44 @@
+package dto;
+
+public class Person {
+
+	private int personId;
+	private String personName;
+	private int personAge;
+	private String personAddress;
+	private long personPhone;
+	
+	public int getPersonId() {
+		return personId;
+	}
+	public void setPersonId(int personId) {
+		this.personId = personId;
+	}
+	public String getPersonName() {
+		return personName;
+	}
+	public void setPersonName(String personName) {
+		this.personName = personName;
+	}
+	public int getPersonAge() {
+		return personAge;
+	}
+	public void setPersonAge(int personAge) {
+		this.personAge = personAge;
+	}
+	public String getPersonAddress() {
+		return personAddress;
+	}
+	public void setPersonAddress(String personAddress) {
+		this.personAddress = personAddress;
+	}
+	public long getPersonPhone() {
+		return personPhone;
+	}
+	public void setPersonPhone(long personPhone) {
+		this.personPhone = personPhone;
+	}
+	
+	
+	
+}
