a15c119294d536007e0e652aea92b72a6b033837
Nagarjuna Yadavalli <86463176+Nagarjunay123@users.noreply.github.com>
2024-04-13 12:58:52 +0530

    Add files via upload

---
+++ Callable Statement/dto/Laptop.java
@@ -0,0 +1,42 @@
+package dto;
+
+public class Laptop {
+		private int laptopId;
+		private String laptopBrand;
+		private double laptopPrice;
+		private String laptopColor;
+		private int laptopRam;
+		public int getLaptopId() {
+			return laptopId;
+		}
+		public void setLaptopId(int laptopId) {
+			this.laptopId = laptopId;
+		}
+		public String getLaptopBrand() {
+			return laptopBrand;
+		}
+		public void setLaptopBrand(String laptopBrand) {
+			this.laptopBrand = laptopBrand;
+		}
+		public double getLaptopPrice() {
+			return laptopPrice;
+		}
+		public void setLaptopPrice(double laptopPrice) {
+			this.laptopPrice = laptopPrice;
+		}
+		public String getLaptopColor() {
+			return laptopColor;
+		}
+		public void setLaptopColor(String laptopColor) {
+			this.laptopColor = laptopColor;
+		}
+		public int getLaptopRam() {
+			return laptopRam;
+		}
+		public void setLaptopRam(int laptopRam) {
+			this.laptopRam = laptopRam;
+		}
+		
+		
+	}
+
